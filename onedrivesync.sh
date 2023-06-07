dd8983f0ef12b3f74a5e4044f520fc2d6274a012
kkirino <kosuke.kirino@gmail.com>
2023-06-07 15:04:57 +0900

    [fix] onedrivesync.sh の修正

--- onedrivesync.sh
+++ onedrivesync.sh
@@ -14,36 +14,64 @@ DIR_DEST=${DIR_ONEDRIVE}/${DIR_SOURCE##*/}
 # usage function を定義
 function usage() {
     cat <<EOF
-usage: onedrivesync [command] [option]
+usage: onedrivesync [command]
 command: push   synchronize source to destination
          pull   synchronize destination to source
-option: -n, --dry-run   perform a trial run with no changes made
+         rm     remove OneDrive directory
 EOF
     exit 1
 }
 
 # エラーの対処
-if [ ! $# -eq 1 ] && [ ! $# -eq 2 ]; then
-    echo 1
+if [ ! $# -eq 1 ]; then
     usage
-elif [ ! "$1" = 'push' ] && [ ! "$1" = 'pull' ]; then
-    echo 2
-    usage
-elif [$# -eq 2 ] && [ ! "$2" = '--dry-run' ] && [ ! "$2" = '-n' ]; then
-    echo 3
+elif [ ! "$1" = 'push' ] && [ ! "$1" = 'pull' ] && [ ! "$1" = 'rm' ]; then
     usage
 elif [ ! -d ${DIR_DEST} ] && [ "$1" = 'pull' ]; then
+    echo "onedrivesync: ${DIR_SOURCE##*/}: no such directory in OneDrive"
+    exit 1
+elif [ ! -d ${DIR_DEST} ] && [ "$1" = 'rm' ]; then
     echo "onedrivesync: ${DIR_SOURCE##*/}: no such directory in OneDrive"
     exit 1
 fi
 
 # push の処理 
 if [ "$1" = 'push' ]; then
-    rsync -auvh --delete ${DIR_SOURCE} ${DIR_ONEDRIVE} $2
-    exit 0
+    # rsync dry run
+    rsync -auvh --delete ${DIR_SOURCE} ${DIR_ONEDRIVE} --dry-run
+    # 実行の可否を問う
+    echo -n Do you really want to run synchronization? [yes/no]: 
+    read input
+    if [ "$input" = "no" ]; then 
+        exit 0
+    elif [ "$input" = "yes" ]; then
+        # rsync 実行
+        rsync -auvh --delete ${DIR_SOURCE} ${DIR_ONEDRIVE}
+        exit 0
+    else
+        echo "onedrivesync: $input: input yes or no"
+        exit 1
+    fi
 # pull の処理
+elif [ "$1" = 'pull' ]; then
+    # rsync dry run
+    rsync -auvh --delete ${DIR_DEST} ${DIR_PULL_TO} --dry-run
+    # 実行の可否を問う
+    echo -n Do you really want to run synchronization? [yes/no]: 
+    read input
+    if [ "$input" = "no" ]; then 
+        exit 0
+    elif [ "$input" = "yes" ]; then
+        # rsync 実行
+        rsync -auvh --delete ${DIR_DEST} ${DIR_PULL_TO}
+        exit 0
+    else
+        echo "onedrivesync: $input: input yes or no"
+        exit 1
+    fi
+# rm の処理
 else
-    rsync -auvh --delete ${DIR_DEST} ${DIR_PULL_TO} $2
+    rm -rf ${DIR_DEST}
     exit 0
 fi
  
